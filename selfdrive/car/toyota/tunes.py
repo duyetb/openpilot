c6f47e321e49c71f6d923fbc6192a2d42e920561
duyetb <60722981+duyetb@users.noreply.github.com>
2021-11-20 21:33:28 -0500

    Update tunes.py

--- selfdrive/car/toyota/tunes.py
+++ selfdrive/car/toyota/tunes.py
@@ -121,8 +121,8 @@ def set_lat_tune(tune, name):
       tune.pid.kiV = [0.05]
       tune.pid.kf = 0.00004
     elif name == LatTunes.PID_J:
-      tune.pid.kpV = [0.19]
-      tune.pid.kiV = [0.02]
+      tune.pid.kpV = [0.1]
+      tune.pid.kiV = [0.01]
       tune.pid.kf = 0.00007818594
     elif name == LatTunes.PID_L:
       tune.pid.kpV = [0.3]
